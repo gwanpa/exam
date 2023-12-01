25c13380eb82974ecd68ed413298fe98645adbe8
learner <144351754+gwanpa@users.noreply.github.com>
2023-11-30 20:34:47 -0800

    README.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# exam
-python codes
---
+++ README.py
@@ -0,0 +1,11 @@
+# exam
+#wap to check wheather the number is positive, negative or zero
+num=int(input("Enter a number: "))
+if (num==0):
+  print("zero")
+elif (num<0):
+  print(num,"is negative number")
+elif (num>0):
+  print(num,"is positive number")
+else:
+  print("invalid")
